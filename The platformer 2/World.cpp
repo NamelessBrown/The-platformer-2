033ab1487877ee4d5e2eefd87a5a5b7681a835f6
Nameless <tyrebrown873@gmail.com>
2020-09-04 18:33:48 -0400

    All day testing led to no where

--- The platformer 2/World.cpp
+++ The platformer 2/World.cpp
@@ -4,7 +4,7 @@
 World::World(sf::RenderWindow& window)
 	:m_window(window)
 {
-	//BuildWorld("");
+
 }
 
 void World::Update(float dt)
@@ -19,19 +19,5 @@ void World::Render()
 //tinyxml!!!
 void World::BuildWorld(const std::string& filename)
 {
-	/* tinyxml2::XMLDocument document;
-	document.LoadFile(filename.c_str());
-	if (document.Error())
-	{
-		std::string error{ "Failed to load file " + filename };
-		throw ParsingException{ error.c_str() };
-	}
-
-	tinyxml2::XMLElement* root = document.RootElement();
-	for (auto element = root->FirstChildElement(); element != nullptr; element = element->NextSiblingElement())
-	{
-		element->Attribute()
-	}
-	*/
 
 }
